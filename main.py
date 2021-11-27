c9570ce1ee521ba079fd386692794f85045b0bc9
Mac Mbizo <macmbizo@gmail.com>
2021-11-27 19:43:19 +0200

    added a way to get the raw data to put in a data frame

--- main.py
+++ main.py
@@ -6,13 +6,27 @@ from time import sleep
 def get_crypto_rates(base_currency='USD', assets='BTC, ETH, XRP' ):
     url = 'https://api.nomics.com/v1/currencies/ticker'
 
-    payload = {'key' : keys.NOMICS_API_KEY, 'convert': base_currency, 'ids': assets 'interval': 'id'}
+    payload = {'key': keys.NOMICS_API_KEY, 'convert': base_currency, 'ids': assets, 'interval': 'id'}
     response = requests.get(url, params=payload)
     data = response.json()
 
-    crypto_currency, cypto_price,crypto_timespace = [], [], []
+    crypto_currency, crypto_price,crypto_timestamp = [], [], []
     
     for asset in data:
-        
-
+        crypto_currency.append(asset['currency'])
+        crypto_price.append(asset['price'])
+        crypto_timestamp.append(asset['price_timestamp'])
     
+    raw_data = {
+        'assets' : crypto_currency,
+        'rates' : crypto_price,
+        'timestamp' : crypto_timestamp
+
+    }
+
+    df = pd.DataFrame(raw_data)
+    print(df)
+    return df
+
+
+get_crypto_rates('ZAR', 'DOT')
